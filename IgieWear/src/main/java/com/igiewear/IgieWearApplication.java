164e1633c4b21877bcc9ddbafb3508abffa93365
Damian Bala <damian.bala@ericsson.com>
2016-11-15 13:53:52 +0100

    Clean SPRING Project

---
+++ IgieWear/src/main/java/com/igiewear/IgieWearApplication.java
@@ -0,0 +1,12 @@
+package com.igiewear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgieWearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgieWearApplication.class, args);
+	}
+}
